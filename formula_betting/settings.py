e6bc1944f7332345cc125a13c0e0f61efd3482a3
Livia <hortenciolima1994@gmail.com>
2025-06-24 12:25:03 -0300

    novo deploy

--- formula_betting/settings.py
+++ formula_betting/settings.py
@@ -1,22 +1,23 @@
 from pathlib import Path
+import django_heroku
 import os
 import dj_database_url
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-DEBUG = os.getenv("DEBUG", "False") == "True"
+# Configura banco do Heroku
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL')
+    )
+}
 
+# Ative as configurações do Heroku
+django_heroku.settings(locals())
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
+# Adicione o host do heroku no ALLOWED_HOSTS
+ALLOWED_HOSTS = ['formula-betting.herokuapp.com', 'localhost']
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w03uu6ljuhjj#3zn#b58=67)8-#4v!sd8up(lp5_(&y&d9z^3y'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['newformula.onrender.com', 'localhost']
+# DEBUG geralmente False em produção
+DEBUG = False
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
